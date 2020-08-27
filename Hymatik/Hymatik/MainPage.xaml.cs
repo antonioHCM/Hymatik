2702cf3d7c444a9ded629cb7f30bf1e68910d32a
N4G4TO <N4G4TO@DESKTOP-OO4IDIC>
2020-08-27 16:31:19 +0200

    pdf conversion

--- Hymatik/Hymatik/MainPage.xaml.cs
+++ Hymatik/Hymatik/MainPage.xaml.cs
@@ -14,5 +14,28 @@ namespace Hymatik
         {
             InitializeComponent();
         }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            ////Input XAML file location
+            //string fileName = "MainPage.xaml";
+            ////Convert XAML file to XPS file
+            //Stream xpsFile = GetXPSDocument(fileName);
+            //if (xpsFile != null)
+            //{
+            //    xpsFile.Position = 0;
+            //    //Initialize XPSToPdfConverter
+            //    Syncfusion.XPS.XPSToPdfConverter converter = new Syncfusion.XPS.XPSToPdfConverter();
+            //    //Convert XPS document into PDF document
+            //    PdfDocument document = converter.Convert(xpsFile);
+            //    //Save the Pdf document
+            //    document.Save("XAMLToPDF.pdf");
+            //    //Close the Pdf document
+            //    document.Close(true);
+            //    //This will open the PDF file so, the result will be seen in default PDF viewer 
+            //    System.Diagnostics.Process.Start("XAMLToPDF.pdf");
+            //}
+            DisplayAlert("Warnning", "Your order has been sent to Hymatik", "Ok");
+        }
     }
 }
